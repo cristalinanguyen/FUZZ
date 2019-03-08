b4f0442f587c123d9d1d2ebe42c9c4fceedc9167
Amelia Jay <31746937+ameliajay@users.noreply.github.com>
2019-03-07 19:56:14 -0800

    Create parser-test.js

---
+++ tests/parser/parser-test.js
@@ -0,0 +1,27 @@
+/* Adapted from Ray Toal's Plainscript parser-test.js
+ * Parser Tests
+ *
+ * Tests that the parser produces the expected abstract syntax tree for a
+ * variety of programs.
+ */
+
+const util = require('util');
+const fs = require('fs');
+const parse = require('../../syntax/parser');
+
+describe('The parser', () => {
+  fs.readdirSync(__dirname).forEach((name) => {
+    if (name.endsWith('.fuzz')) {
+      test(`produces the correct AST for ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          const ast = parse(input);
+          const astText = util.inspect(ast, { depth: null });
+          fs.readFile(`${__dirname}/${name.slice(0, -3)}ast`, 'utf-8', (_err, expected) => {
+            expect(astText).toEqual(expected.trim());
+            done();
+          });
+        });
+      });
+    }
+  });
+});
