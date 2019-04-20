933325d193dd2525c0982d5d79de395fdb1043a8
stprochnow64 <stprochnow64@gmail.com>
2019-04-20 13:55:26 -0600

    Delete chill-statement.js

--- ast/chill-statement.js
+++
@@ -1,7 +0,0 @@
-module.exports = class ChillStatement {
-    analyze(context) {
-        if (!context.inLoop) {
-            throw new Error('Break statement is not inside the loop');
-        }
-    }
-}
