334698fe9d60b3fa68cc6d2b631a1d5f100943c1
Amelia Jay <31746937+ameliajay@users.noreply.github.com>
2019-04-22 13:43:55 -0700

    Update index.js

--- ast/index.js
+++ ast/index.js
@@ -121,7 +121,7 @@ class WhileExp {
 }
 
 module.exports = {
-  ArrayExp, ArrayType, Assignment, BinaryExp, Call, Chill, Dict, Field,
+  ArrayExp, ArrayType, AssignmentStatement, BinaryExp, Call, Chill, Dict, Field,
   ForExp, Func, IdExp, IphExp, Literal, MemberExp, NegationExp, Param,
   SetExp, SetType, SubscriptedExp, TypeDec, Variable, WhileExp,
 };
