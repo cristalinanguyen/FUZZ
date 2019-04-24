86595f29878fea93211cb314bf053fc3be2202a2
stprochnow64 <stprochnow64@gmail.com>
2019-04-23 18:42:42 -0700

    Update grammar.test.js

--- Syntax/__tests__/grammar.test.js
+++ Syntax/__tests__/grammar.test.js
@@ -8,53 +8,9 @@
 const syntaxCheck = require('../syntax-checker');
 
 const program = String.raw`
+
 yoo! tests are so cool !
 
-fuzz int x = 10
-fuzz int y = 20
-
-fuzz str test = "Fuzzies!"
-fuzz bool question = cozy
-fuzz bool bad = not_cozy
-
-fuzzArr[str] strArr = ["a", "b", "c"]
-fuzzArr[int] numArr = [0, 1, 2, 3]
-
-fuzzDict<str, int> testDict = ["Op Systems": 545, "Compilers": 240, "Graphics: 940]
-
-function dondi() 
-  x = 12
-  y = 2
-  returnt x
-~
-
-iph (x >= y) 
-  print ("Hi there /n")
-~
-
-iph (x <= y)
-  print ("Wrong")
-~
-elz iph (x == y)
-  print ("Nope /t")
-~
-elz
-  print ("WOOHOO!")
-
-iph (bad)
-  fuzz q = x * 3
-  fuzz w = y / 10
-  fuzz v = x ** 3
-~
-elz
-  fuzz r = x + y
-~
-
-while (question)
-  x = x - 4
-  iph (x == 0)
-    chill
-  ~
 `;
 
 describe('The syntax checker', () => {
