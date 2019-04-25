17a103297519314a9d00edaccec1495d961f0020
stprochnow64 <stprochnow64@gmail.com>
2019-04-24 22:38:58 -0700

    Update builtins.js

--- semantics/builtins.js
+++ semantics/builtins.js
@@ -7,7 +7,7 @@ const BoolType = new Type('boolean');
 const standardFunctions = [
   new Func('show', [new Param('s', StringType)]),
   new Func('length', [new Param('s', StringType)], IntType),
-  new Func('cuddle', [new Param('s', StringType), new Field('t', StringType),], StringType),
+  new Func('cuddle', [new Param('s', StringType), new Param('t', StringType),], StringType),
 ];
 
 module.exports = { IntType, StringType, BoolType, standardFunctions };
