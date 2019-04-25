295997f76ee39f72126b82e62dfffc9c0d7f5769
Annie <aflora1@lion.lmu.edu>
2019-04-25 15:58:40 -0700

    Update grammar_errors.test.js

--- Syntax/__tests__/grammar_errors.test.js
+++ Syntax/__tests__/grammar_errors.test.js
@@ -1,15 +1,16 @@
 const syntaxCheck = require('../syntax_checker');
 
 const errors = [
-  ['keyword as id', 'fuzz num iph = 5'],
-  ['unclosed paren', 'fuzz num = (2 * 3'],
-  ['unknown operator', 'x = 2^4'],
-  ['unclosed comment', 'heres the thing: I like chai'],
-  ['illegal start to comment', 'here: I like iced chai !!'],
-  ['illegal start variable name', 'fuzz str $$ = "hey"'],
-  ['unclosed array', 'fuzz Arr<num> class = [185,186,401'],
-  ['unclosed dictionary', 'fuzz Dict<num> coffee_prices = {chai: 3.50, tea: 2.50, cold_brew: 4.00'],
-  
+  ['keyword as id', 'yoo! fuzz num iph = 5'],
+  ['unclosed paren', 'yoo! fuzz num = (2 * 3'],
+  ['unknown operator', 'yoo! x = 2^4'],
+  ['unclosed comment', 'yoo! heres the thing: I like chai'],
+  ['illegal start to comment', 'yoo! here: I like iced chai !!'],
+  ['illegal start variable name', 'yoo! fuzz str $$ = "hey"'],
+  ['unclosed array', 'yoo! fuzz Arr<num> class = [185,186,401'],
+  ['unclosed dictionary', 'yoo! fuzz Dict<num> coffee_prices = {chai: 3.50, tea: 2.50, cold_brew: 4.00'],
+  ['method body not enclosed', 'yoo! function cozy_coding() returnt cozy'],
+  ['statement not enclosed', 'yoo! iph(coffee_cart) returnt "chai"']
 ];
 
 describe('The syntax checker', () => {
