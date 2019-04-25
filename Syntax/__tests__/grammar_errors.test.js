08f31b3f99549dbab7d4452725a4a5236c8bd7ba
Annie <aflora1@lion.lmu.edu>
2019-04-25 15:39:00 -0700

    Update grammar_errors.test.js

--- Syntax/__tests__/grammar_errors.test.js
+++ Syntax/__tests__/grammar_errors.test.js
@@ -1,7 +1,12 @@
 const syntaxCheck = require('../syntax_checker');
 
 const errors = [
-  
+  ['keyword as id', 'fuzz num iph = 5'],
+  ['unclosed paren', 'fuzz num = (2 * 3'],
+  ['unknown operator', 'x = 2^4'],
+  ['unclosed comment', 'heres the thing: I like chai'],
+  ['illegal start to comment', 'here: I like iced chai !!'],
+  ['illegal start variable name', 'fuzz str $$ = "hey"']
 ];
 
 describe('The syntax checker', () => {
