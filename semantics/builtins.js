ffff087c9bfb2d458b53c7324da06d1932c5d7df
stprochnow64 <stprochnow64@gmail.com>
2019-04-30 13:14:04 -0700

    change int to num

--- semantics/builtins.js
+++ semantics/builtins.js
@@ -1,13 +1,13 @@
 const { Func, Param, Type } = require('../ast');
 
-const IntType = new Type('int');
+const NumType = new Type('num');
 const StringType = new Type('string');
 const BoolType = new Type('boolean');
 
 const standardFunctions = [
   new Func('show', [new Param('s', StringType)]),
-  new Func('length', [new Param('s', StringType)], IntType),
+  new Func('length', [new Param('s', StringType)], NumType),
   new Func('cuddle', [new Param('s', StringType), new Param('t', StringType),], StringType),
 ];
 
-module.exports = { IntType, StringType, BoolType, standardFunctions };
+module.exports = { NumType, StringType, BoolType, standardFunctions };
