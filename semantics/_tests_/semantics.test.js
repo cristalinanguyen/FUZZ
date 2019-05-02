e39abfe14a8b8bde2c6cff70341bc388b3854fac
stprochnow64 <stprochnow64@gmail.com>
2019-05-02 15:38:48 -0700

    Update semantics.test.js

--- semantics/_tests_/semantics.test.js
+++ semantics/_tests_/semantics.test.js
@@ -43,8 +43,7 @@ fuzz str dict_test = age.Annie
 fuzz bool s = (x >= 1)
 fuzz bool t = (y <= 0)
 fuzz bool u = (x < 15)
-fuzz bool v = (x not= 5)
-`;
+fuzz bool v = (x not= 5)`;
 
 describe('The semantic analyzer', () => {
   test('accepts the mega program with all syntactic forms', (done) => {
