89dcf02ab2c3d35f6160a67ece85c4ccdc9822bb
Amelia Jay <31746937+ameliajay@users.noreply.github.com>
2019-05-07 22:06:47 -0700

    Update builtins.js

--- semantics/builtins.js
+++ semantics/builtins.js
@@ -5,9 +5,9 @@ const StringType = new Type('string');
 const BoolType = new Type('boolean');
 
 const standardFunctions = [
-  new Func('show', [new Param('s', StringType)]),
-  new Func('show', [new Param('b', BoolType)]),
-  new Func('show', [new Param('n', NumType)]),
+  new Func('showStr', [new Param('s', StringType)]),
+  new Func('showBool', [new Param('b', BoolType)]),
+  new Func('showNum', [new Param('n', NumType)]),
   new Func('length', [new Param('s', StringType)], NumType),
   new Func('cuddle', [new Param('s', StringType), new Param('t', StringType),], StringType),
 ];
